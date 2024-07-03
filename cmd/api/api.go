b157820c98790fe8ba35979c5742d282e806c177
Kranti <darkphoton20@gmail.com>
2024-07-04 05:09:27 +0800

    Added setup for project, card payment intent apis

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"time"
 )
 
 // Config
@@ -19,6 +21,22 @@ type config struct {
 		secret string
 		key    string
 	}
+}
+
+// Serve
+func (app *application) serve() error {
+	srv := &http.Server{
+		Addr:              fmt.Sprintf(":%d", app.config.port),
+		Handler:           app.routes(),
+		IdleTimeout:       30 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
+		WriteTimeout:      5 * time.Second,
+	}
+
+	app.infoLog.Println((fmt.Sprintf("Starting Backend API Server in %s mode on port %d", app.config.env, app.config.port)))
+
+	return srv.ListenAndServe()
 }
 
 // Application
@@ -47,5 +65,16 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	app := &application
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	err := app.serve()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/darkphotonKN/virtual-credit-card-server
 
 go 1.22.3
 
-require github.com/stripe/stripe-go/v72 v72.122.0 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.1.0 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+	github.com/stripe/stripe-go/v72 v72.122.0 // indirect
+)
